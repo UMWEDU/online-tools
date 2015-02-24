17fcf3369ac7dc2a2fda4b66c4bf222776791918
cgrymala <curt_grymala@yahoo.com>
2015-02-24 16:10:51 -0500

    Create initial version
    Implement initial version of plugin
    Modify readme to match WordPress readme format
    Add all icons
    Add CSS file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# online-tools
-Implements the online tools toolbar at the top of the UMW site
+=== UMW Online Tools ===
+Contributors: cgrymala
+Donate link: http://giving.umw.edu/
+Requires at least: 3.9.1
+Tested up to: 4.1.1
+Stable tag: 0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Implements the online tools toolbar shown at the top of the UMW website
+
+== Description ==
+
+Once activated, this plugin will automatically insert the Online Tools toolbar at the top of all pages on the site. The list of tools can optionally be filtered.
+
+== Installation ==
+
+1. Upload the `umw-online-tools` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+= Can I remove or add icons? =
+
+Yes, the list of icons can be filtered using the `umw-online-tools-icons` filter.
+
+== Changelog ==
+
+= 0.2 =
+* This is the first version of this as a standalone plugin
---
+++ classes/class-umw-online-tools.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Implements the UMW Online Tools class
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+  die( 'You should not access this file directly' );
+}
+
+class UMW_Online_Tools {
+  public $v = '0.2';
+  public $icons = array();
+
+  function __construct() {
+    add_action( 'after_setup_theme', array( $this, 'after_setup_theme' ) );
+    add_action( 'init', array( $this, 'gather_icons' ) );
+  }
+
+  function after_setup_theme() {
+    if ( ! function_exists( 'genesis' ) ) {
+      return false;
+    }
+
+    if ( has_action( 'genesis_before_header', 'umw_do_help_section' ) ) {
+      remove_action( 'genesis_before_header', 'umw_do_help_section' );
+    }
+
+    add_action( 'genesis_before_header', array( $this, 'do_toolbar' ) );
+    add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
+  }
+
+  function enqueue_styles() {
+    wp_enqueue_style( 'umw-online-tools', plugins_url( 'umw-online-tools.css', dirname( __FILE__ ) ), array(), $this->v, 'all' );
+  }
+
+  function gather_icons() {
+    $this->icons = apply_filters( 'umw-online-tools-icons', array(
+      0 => array(
+        'icon' => 'eaglenet',
+        'link' => 'https://eaglenet.umw.edu/',
+        'name' => 'EagleNet',
+      ),
+      1 => array(
+        'icon' => 'banner',
+        'link' => 'http://technology.umw.edu/hss/banner/',
+        'name' => 'Banner',
+      ),
+      2 => array(
+        'icon' => 'canvas',
+        'link' => 'https://canvas.umw.edu/',
+        'name' => 'Canvas',
+      ),
+      3 => array(
+        'icon' => 'email',
+        'link' => 'http://webmail.umw.edu/',
+        'name' => 'Email',
+      ),
+      4 => array(
+        'icon' => 'library',
+        'link' => 'http://libraries.umw.edu/',
+        'name' => 'Library',
+      ),
+      5 => array(
+        'icon' => 'eagleone',
+        'link' => 'https://eagleone-sp.blackboard.com/eaccounts',
+        'name' => 'EagleOne',
+      ),
+      6 => array(
+        'icon' => 'mytime',
+        'link' => 'https://umw.kronos.net/wfc/navigator/logon',
+        'name' => 'MyTime',
+      ),
+      7 => array(
+        'icon' => 'eagleeye',
+        'link' => 'http://eagleeye.umw.edu/',
+        'name' => 'EagleEye',
+      ),
+      8 => array(
+        'icon' => 'password',
+        'link' => 'http://password.umw.edu/',
+        'name' => 'Passwords',
+      ),
+      9 => array(
+        'icon' => 'directory',
+        'link' => 'http://umw.edu/directory',
+        'name' => 'Directory',
+      ),
+      10 => array(
+        'icon' => 'starfish',
+        'link' => 'https://umw.starfishsolutions.com/starfish-ops/session/casLogin.html',
+        'name' => 'Starfish',
+      ),
+      11 => array(
+        'icon' => 'links',
+        'link' => 'http://www.umw.edu/resources/',
+        'name' => 'Helpful Links',
+      ),
+    ) );
+  }
+
+  function do_toolbar() {
+    $output = '';
+    $format = '<li><a href="%1$s"><img src="%2$s?v=%3$s" alt=""/>%4$s</a></li>';
+    foreach ( $this->icons as $i ) {
+      if ( ! esc_url( $i['icon'] ) ) {
+        $i['icon'] = plugins_url( sprintf( 'images/icons/svg-online-tools/24px-blue/umwicon-%1$s.png', $i['icon'] ), dirname( __FILE__ ) );
+      }
+      $output .= sprintf( $format, esc_url( $i['link'] ), $i['icon'], $this->v, $i['name'] );
+    }
+    printf( '<aside class="umw-helpful-links"><ul class="umw-tools">%s</ul><br style="clear:both;"/></aside>', $output );
+  }
+}
