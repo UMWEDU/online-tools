0cd1dea9021eb690a0f114b5d322d6ca38a602b2
Curtiss Grymala <curt_grymala@yahoo.com>
2015-08-13 00:13:16 -0400

    Change menu header for toolbar on mobile

--- scripts/umw-online-tools.js
+++ scripts/umw-online-tools.js
@@ -8,7 +8,7 @@ jQuery( function() {
 			if ( document.querySelectorAll( '.header-widget-area .mega-menu-wrap .umw-helpful-links' ).length >= 1 ) {
 				return;
 			}
-			jQuery( '.header-widget-area .mega-menu-wrap > ul.mega-menu' ).append( '<li class="mega-menu-item mega-menu-item-has-children mega-menu-item-umw-online-tools"><a href="#">Tools</a><ul class="mega-sub-menu"><li></li></ul></li>' );
+			jQuery( '.header-widget-area .mega-menu-wrap > ul.mega-menu' ).append( '<li class="mega-menu-item mega-menu-item-has-children mega-menu-item-umw-online-tools"><a href="#">Tools & Search</a><ul class="mega-sub-menu"><li></li></ul></li>' );
 			jQuery( '.mega-menu-item-umw-online-tools > a' ).on( 'click', function() { jQuery( this ).closest( '.mega-menu-item' ).toggleClass( 'mega-toggle-on' ); return false; } );
 			jQuery( '.header-widget-area .mega-menu-item-umw-online-tools .mega-sub-menu li' ).append( jQuery( '.umw-helpful-links' ) ).append( jQuery( '.umw-header-bar' ) );
 			jQuery( '.umw-audience-menu' ).insertBefore( jQuery( '.umw-search-wrapper' ) );
