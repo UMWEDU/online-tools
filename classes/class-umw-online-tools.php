022c593db499e722105ec8694ef44e87cf912e34
Curtiss Grymala <curt_grymala@yahoo.com>
2015-09-04 12:02:28 -0400

    Increment version number; add IE-fix styles for search

--- classes/class-umw-online-tools.php
+++ classes/class-umw-online-tools.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Implements the UMW Online Tools class
- * @version 0.4.5.6
+ * @version 0.5
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class UMW_Online_Tools {
-  public $v = '0.4.5.6';
+  public $v = '0.5';
   public $icons = array();
   public $options = array();
 
--- online-tools.php
+++ online-tools.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: UMW Online Tools
  * Description: Implements the toolbar for Online Tools at the UMW website
- * Version: 0.4.5
+ * Version: 0.5
  * Author: cgrymala
  * License: GPL2
  */
