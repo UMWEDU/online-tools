deb6f9539db20c6392f3e10332c270e5e9e0b466
Curtiss Grymala <curt_grymala@yahoo.com>
2016-03-08 09:56:32 -0500

    Remove potentially-breaking debug information

--- scripts/umw-online-tools.js
+++ scripts/umw-online-tools.js
@@ -8,7 +8,6 @@ jQuery( function() {
 			if ( document.querySelectorAll( '.header-widget-area .mega-menu-wrap .umw-helpful-links' ).length >= 1 ) {
 				return;
 			}
-			console.log( 'The mega menu toggle is reporting as visible, so things will be moved around' );
 			jQuery( '.header-widget-area .mega-menu-wrap > ul.mega-menu' ).append( '<li class="mega-menu-item mega-menu-item-has-children mega-menu-item-umw-online-tools"><a href="#" class="tools-search-toggle mega-menu-link">Tools & Search</a><ul class="mega-sub-menu"><li></li></ul></li>' );
 			jQuery( '.mega-menu-item-umw-online-tools > a' ).on( 'click', function() { jQuery( this ).closest( '.mega-menu-item' ).toggleClass( 'mega-toggle-on' ); return false; } );
 			jQuery( '.header-widget-area .mega-menu-item-umw-online-tools .mega-sub-menu li' ).append( jQuery( '.umw-helpful-links' ) ).append( jQuery( '.umw-header-bar' ) );
@@ -17,7 +16,6 @@ jQuery( function() {
 			if ( document.querySelectorAll( 'body > .umw-helpful-links' ).length >= 1 ) {
 				return;
 			}
-			console.log( 'Things are being put back in place because the mega menu toggle is not reporting as visible' );
 			jQuery( 'body' ).prepend( jQuery( '.umw-header-bar' ) ).prepend( jQuery( '.umw-helpful-links' ) );
 			jQuery( '.umw-audience-menu' ).insertBefore( jQuery( '.umw-header-bar > .wrap .desktop-clear' ) );
 			jQuery( '.mega-menu-item-umw-online-tools' ).remove();
